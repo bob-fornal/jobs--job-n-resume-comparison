70c3f8855490f7b923ca35defa4bd8b60055816c
Bob Fornal <bob.fornal@leadingedje.com>
2025-02-01 15:21:48 -0500

    Unit Tests & Visual Adjustments / Color

--- src/app/core/services/storage.service.spec.ts
+++ src/app/core/services/storage.service.spec.ts
@@ -13,4 +13,27 @@ describe('StorageService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('expects "getDarkMode to return false if nothing in local storage', () => {
+    spyOn(service.localstorage, 'getItem').and.returnValue(null);
+    spyOn(service.localstorage, 'setItem').and.stub();
+
+    const result: boolean = service.getDarkMode();
+    expect(result).toEqual(false);
+    expect(service.localstorage.setItem).toHaveBeenCalledWith('job-squid--dark-mode', 'false');
+  });
+
+  it('expects "getDarkMode" to return true of mode is true', () => {
+    spyOn(service.localstorage, 'getItem').and.returnValue('true');
+
+    const result: boolean = service.getDarkMode();
+    expect(result).toEqual(true);
+  });
+
+  it('expects "getDarkMode" to return false of mode is false', () => {
+    spyOn(service.localstorage, 'getItem').and.returnValue('false');
+
+    const result: boolean = service.getDarkMode();
+    expect(result).toEqual(false);
+  });
 });
