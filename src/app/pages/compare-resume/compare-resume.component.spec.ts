0ab1d80eeadd4e3fb6de5b529ac169a18017f26d
Bob Fornal <bob.fornal@leadingedje.com>
2025-02-04 21:11:06 -0500

    Compare Unit Test Improvement

--- src/app/pages/compare-resume/compare-resume.component.spec.ts
+++ src/app/pages/compare-resume/compare-resume.component.spec.ts
@@ -47,4 +47,64 @@ describe('CompareResumeComponent', () => {
     component.init();
     expect(component['storage'].getResumes).toHaveBeenCalled();
   });
+
+  it('expects "textareaAdjust" to adjust height for a non-native element', () => {
+    const event: any = {
+      nativeElement: {
+        scrollHeight: 100,
+        style: {
+          height: '',
+        },
+      },
+    };
+
+    component.textareaAdjust(event, true);
+    expect(event.nativeElement.style.height).toEqual('116px');
+  });
+
+  it('expects "textareaAdjust" to adjust height for a native element', () => {
+    const event: any = {
+      target: {
+        scrollHeight: 200,
+        style: {
+          height: '',
+        },
+      },
+    };
+
+    component.textareaAdjust(event);
+    expect(event.target.style.height).toEqual('216px');
+  });
+
+  it('expects "doesValidationDisable" to return true of resumeNameLength is true', () => {
+    component.validationChecks['resumeNameLength'] = true;
+    component.validationChecks['resumeContentLength'] = false;
+
+    const result: boolean = component.doesValidationDisable();
+    expect(result).toEqual(true);
+  });
+
+  it('expects "doesValidationDisable" to return true of resumeContentLength is true', () => {
+    component.validationChecks['resumeNameLength'] = false;
+    component.validationChecks['resumeContentLength'] = true;
+
+    const result: boolean = component.doesValidationDisable();
+    expect(result).toEqual(true);
+  });
+
+  it('expects "isRunComparisonDisabled" to return true of doesValidationDisable is true', () => {
+    component.validationChecks['jobContentLength'] = false;
+    spyOn(component, 'doesValidationDisable').and.returnValue(true);
+
+    const result: boolean = component.isRunComparisonDisabled();
+    expect(result).toEqual(true);
+  });
+
+  it('expects "isRunComparisonDisabled" to return true of jobContentLength is true', () => {
+    component.validationChecks['jobContentLength'] = true;
+    spyOn(component, 'doesValidationDisable').and.returnValue(false);
+
+    const result: boolean = component.isRunComparisonDisabled();
+    expect(result).toEqual(true);
+  });
 });
