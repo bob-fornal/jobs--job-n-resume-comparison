e347a12c9b2ebd45d16432b81ad375ed082fb9bb
Bob Fornal <bob.fornal@leadingedje.com>
2025-02-20 15:50:22 -0500

    Custom FavIcons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,36 @@ import { Component } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.css'
 })
-export class AppComponent {}
+export class AppComponent {
+
+  private document: any = window.document;
+  private location: any = window.location;
+
+  private environments: any = [
+    { search: 'localhost', icon: 'favicon--localhost.ico' },
+    { search: 'dev', icon:  'favicon--dev.ico' },
+    { search: 'pre-prod', icon:  'favicon--pre-prod.ico' },
+    { search: 'production', icon:  'favicon--prod.ico' },
+  ];
+  
+  constructor() {
+    document.querySelector
+
+    const faviconElement = this.document.querySelector('#favicon');
+    const url: string = this.location.href;
+    console.log(faviconElement);
+    console.log(url);
+
+    for (let i = 0, len = this.environments.length; i < len; i++) {
+      const environment = this.environments[i];
+      if (environment.search === 'production') {
+        faviconElement.href = environment.icon;
+      } else {
+        if (url.includes(environment.search)) {
+          faviconElement.href = environment.icon;
+          break;
+        }
+      }
+    }
+  }
+}
