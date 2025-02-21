c33938b6c66b24433771b813b5118df03da097a9
Bob Fornal <bob.fornal@leadingedje.com>
2025-02-21 09:14:03 -0500

    Moved Code from App to Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FaviconService } from './core/services/favicon.service';
 
 @Component({
   selector: 'app-root',
@@ -9,34 +10,10 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
 
-  private document: any = window.document;
-  private location: any = window.location;
-
-  private environments: any = [
-    { search: 'localhost', icon: 'favicon--localhost.ico' },
-    { search: 'dev', icon:  'favicon--dev.ico' },
-    { search: 'pre-prod', icon:  'favicon--pre-prod.ico' },
-    { search: 'production', icon:  'favicon--prod.ico' },
-  ];
   
-  constructor() {
-    document.querySelector
-
-    const faviconElement = this.document.querySelector('#favicon');
-    const url: string = this.location.href;
-    console.log(faviconElement);
-    console.log(url);
-
-    for (let i = 0, len = this.environments.length; i < len; i++) {
-      const environment = this.environments[i];
-      if (environment.search === 'production') {
-        faviconElement.href = environment.icon;
-      } else {
-        if (url.includes(environment.search)) {
-          faviconElement.href = environment.icon;
-          break;
-        }
-      }
-    }
+  constructor(
+    faviconService: FaviconService,
+  ) {
+    faviconService.init();
   }
 }
