578fba8e8ff31b4b39fba694304a41e34d32ccd5
Bob Fornal <bob.fornal@leadingedje.com>
2025-03-11 12:49:09 -0400

    Tags

--- src/app/pages/job-applications/job-applications.component.ts
+++ src/app/pages/job-applications/job-applications.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 
+import jobTags from '../../core/constants/job-application.tags.json';
+
 import { JobApplication } from '../../core/interfaces/job-application';
+import { Tag } from '../../core/interfaces/tag';
 
 @Component({
   selector: 'app-job-applications',
@@ -13,7 +16,13 @@ export class JobApplicationsComponent {
 
   applications: Array<JobApplication> = [];
 
+  tags: Array<Tag> = jobTags
+
   navigate = (type: string): void => {
     // TODO
+  };
+
+  getTagStyle = (tag: Tag): string => {
+    return `--mdc-chip-elevated-container-color: ${tag.backgroundColor}; --color-light-foreground: ${tag.foregroundColor}; --mdc-chip-outline-color: ${tag.foregroundColor}; --mdc-chip-outline-width: 2px;`;
   };
 }
