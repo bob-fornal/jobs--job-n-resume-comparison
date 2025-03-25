9496a589b057ad73071f3413794908d98dd52468
Bob Fornal <bob.fornal@leadingedje.com>
2025-03-25 16:18:11 -0400

    Visuals in Place

--- src/app/pages/job-applications/ja-view-tracking/ja-view-tracking.component.ts
+++ src/app/pages/job-applications/ja-view-tracking/ja-view-tracking.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 
 import { JobApplicationsService } from '../job-applications.service';
-import { JobApplication } from '../../../core/interfaces/job-application';
+import { JobActivity, JobApplication } from '../../../core/interfaces/job-application';
 
 import { Tag } from '../../../core/interfaces/tag';
 
@@ -52,6 +52,7 @@ export class JAViewTrackingComponent {
     const emptyTag: Tag = { title: '', backgroundColor: '', foregroundColor: '' };
     const dialogRef = this.dialog.open(JsTrackingModalComponent, {
       data: {
+        index: this.index,
         title: 'Add',
         datetimestamp: '',
         description: '',
@@ -68,6 +69,40 @@ export class JAViewTrackingComponent {
   };
 
   deleteTrackingItem = (index: number): void => {
-    //
+    const applications: Array<JobApplication> = this.service.structure();
+    const application: JobApplication = applications[this.index];
+    const tracking: Array<JobActivity> = application.tracking;
+    tracking.splice(index, 1);
+    tracking.sort((a: JobActivity, b: JobActivity) => {
+      return +a.datetimestamp - +b.datetimestamp;
+    });
+    this.service.saveApplications(applications);
+  };
+
+  isIndexOdd = (index: number): boolean => {
+    if (index === 0) return false;
+    return index % 2 !== 0;
+  }
+
+  getTrackingStyle(item: JobActivity): string {
+    return `
+      color: ${item.tag.foregroundColor};
+      background-color: ${item.tag.backgroundColor};
+      border: 2px solid ${item.tag.foregroundColor};
+    `;
+  }
+
+  getDatetime(datetimestamp: string): string {
+    const date = new Date(datetimestamp);
+
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+
+    const formattedDateTime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    return formattedDateTime;
   };
 }
--- src/app/pages/job-applications/js-tracking-modal/js-tracking-modal.component.ts
+++ src/app/pages/job-applications/js-tracking-modal/js-tracking-modal.component.ts
@@ -1,6 +1,12 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
+import { JobApplicationsService } from '../job-applications.service';
+
+import { JobActivity, JobApplication } from '../../../core/interfaces/job-application';
+import tags from '../../../core/constants/job-application.tags.json';
+import { Tag } from '../../../core/interfaces/tag';
+
 @Component({
   selector: 'app-js-add-tracking-modal',
   standalone: false,
@@ -12,14 +18,53 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 export class JsTrackingModalComponent {
   readonly dialogRef = inject(MatDialogRef<JsTrackingModalComponent>);
   readonly data = inject<any>(MAT_DIALOG_DATA);
+  readonly service = inject(JobApplicationsService);
+
+  tags: Array<Tag> = tags;
+  tagSelected = 0;
 
   datetimeValue: Date = new Date();
+  description = '';
+  tag: Tag = this.tags[this.tagSelected];
   
   save(): void {
+    const applications: Array<JobApplication> = this.service.structure();
+    const application: JobApplication = applications[this.data.index];
+    const tracking: Array<JobActivity> = application.tracking;
+    tracking.push({
+      datetimestamp: this.toDatetimestamp(this.datetimeValue),
+      description: this.description,
+      tag: this.tag,
+    })
+    tracking.sort((a: JobActivity, b: JobActivity) => {
+      return +a.datetimestamp - +b.datetimestamp;
+    });
+    this.service.saveApplications(applications);
     this.dialogRef.close();
   }
 
   cancel(): void {
     this.dialogRef.close();
+  }
+
+  getTagStyle(tag: Tag) {
+    return `color: ${tag.foregroundColor}; background-color: ${tag.backgroundColor};`;
+  }
+
+  selectTag(index: number) {
+    this.tagSelected = index;
+    this.tag = this.tags[index];
+  }
+
+  toDatetimestamp(date: Date): string {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+
+    const formattedDateTime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    return formattedDateTime;
   }
 }
