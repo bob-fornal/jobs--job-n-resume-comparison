4e7bac19bc1b0d6622b338168d4499bd65b112d2
Bob Fornal <bob.fornal@leadingedje.com>
2025-04-14 10:28:55 -0400

    Cleanup & Functionality

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -10,23 +10,25 @@ export class AboutComponent {
   public position = 30;
   private leftMinimum = 620;
   private rightMaximum = 30;
-  private step = 20;
+  private step = 25;
   public animate = false;
 
   @HostListener('document:keydown', ['$event'])
   handleKeyDown(event: KeyboardEvent) {
     if (this.animate === true) return;
 
+    const keys: Array<string> = ['ArrowLeft', 'ArrowRight', 'ArrowDown', 'ArrowUp'];
+    if (keys.includes(event.key) === false) return;
+
+    this.setAnimation();
     switch (event.key) {
       case 'ArrowLeft':
         if (this.position + this.step > this.leftMinimum) return;
         this.position += this.step;
-        this.setAnimation();
         break;
       case 'ArrowRight':
         if (this.position - this.step < this.rightMaximum) return;
         this.position -= this.step;
-        this.setAnimation();
     }
   }
 
