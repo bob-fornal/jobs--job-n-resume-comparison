9453e80c7403c96d039e897d397796d7e5b35d4c
Bob Fornal <bob.fornal@leadingedje.com>
2025-04-14 14:23:13 -0400

    Functional Easter Egg

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -7,33 +7,43 @@ import { Component, HostListener } from '@angular/core';
   styleUrl: './about.component.css',
 })
 export class AboutComponent {
-  public position = 30;
+  public position = 40;
   private leftMinimum = 620;
-  private rightMaximum = 30;
+  private rightMaximum = 40;
   private step = 25;
-  public animate = false;
+
+  public animateWalker = false;
+  public animateWobble = false;
 
   @HostListener('document:keydown', ['$event'])
   handleKeyDown(event: KeyboardEvent) {
-    if (this.animate === true) return;
+    if (this.animateWalker === true) return;
 
-    const keys: Array<string> = ['ArrowLeft', 'ArrowRight', 'ArrowDown', 'ArrowUp'];
+    const keys: Array<string> = ['ArrowLeft', 'ArrowRight', 'ArrowDown', 'ArrowUp', 'a', 'w', 's', 'z'];
     if (keys.includes(event.key) === false) return;
 
-    this.setAnimation();
+    this.setAnimateWalker();
     switch (event.key) {
       case 'ArrowLeft':
+      case 'a':
         if (this.position + this.step > this.leftMinimum) return;
         this.position += this.step;
         break;
       case 'ArrowRight':
+      case 's':
         if (this.position - this.step < this.rightMaximum) return;
+        if (this.position - this.step === this.rightMaximum) this.setAnimateWobble();
         this.position -= this.step;
     }
   }
 
-  setAnimation(): void {
-    this.animate = true;
-    setTimeout(() => this.animate = false, 500);
+  setAnimateWalker(): void {
+    this.animateWalker = true;
+    setTimeout(() => this.animateWalker = false, 500);
+  }
+
+  setAnimateWobble(): void {
+    this.animateWobble = true;
+    setTimeout(() => this.animateWobble = false, 3000);
   }
 }
